b2de4bd4138bee9dcc2656ca15eb6b26c02bc32b
Neo6424 <129708429+Neo6424@users.noreply.github.com>
2026-04-11 22:40:47 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -3,7 +3,7 @@
 int print_2d_arr(int *rows[],int row_size, int col_size){
     for (int i =0; i<row_size; i++){
         for (int j =0; j<col_size; j++){
-            printf("%d ",*(rows+col_size*i +j));
+            printf("%d ",*(*(rows+i)+j));
         }
         printf("\n");
     }
@@ -13,5 +13,4 @@ int main() {
     int arr[2][3] = {1,2,3,4,5,6};
     int *rows[2] = {arr[0], arr[1]};
     print_2d_arr(rows,2,3);
-
 }
